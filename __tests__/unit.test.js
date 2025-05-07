906301fcc72cb9bab17a46f4204464ba7f8448a5
fleazhang <fez008@ucsd.edu>
2025-05-06 20:12:17 -0700

    testing branch: should be wrong

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,88 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+test('valid phone number test 1', () => {
+  // TODO
+  expect(isPhoneNumber('626-789-0983')).toBe(true);
+});
+
+test('valid phone number test 2', () => {
+  expect(isPhoneNumber('(858) 379-0230')).toBe(true);
+})
+
+test('invalid phone number test 1', () => {
+  expect(isPhoneNumber('123456789')).toBe(false);
+});
+
+test('invalid phone number test 2', () => {
+  expect(isPhoneNumber('yourmomwashere')).toBe(false);
+});
+
+test('valid email test 1', () => {
+  expect(isEmail('yugao123@yahoo.com')).toBe(true);
+});
+
+test('valid email test 2', () => {
+  expect(isEmail('fez008@ucsd.edu')).toBe(true);
+});
+
+test('invalid email test 1', () => {
+  expect(isEmail('gurt&.gmail.com')).toBe(false);
+});
+
+test('invalid email test 2', () => {
+  expect(isEmail('nonsensebysabrina@gmail,com')).toBe(false);
+});
+
+test('valid strong password test 1', () => {
+  expect(isStrongPassword('Gurtwashere')).toBe(true);
+});
+
+test('valid strong password test 2', () => {
+  expect(isStrongPassword('Nonsense_123')).toBe(true);
+});
+
+test('invalid strong password test 1', () => {
+  expect(isStrongPassword('nonsensebysabrina@gmail,com')).toBe(false);
+});
+
+test('invalid strong password test 2', () => {
+  expect(isStrongPassword('AHHHHHHHHHHHHHHHHHHHHHHHH')).toBe(false);
+});
+
+test('valid date test 1', () => {
+  expect(isDate('04/1/2010')).toBe(true);
+});
+
+test('valid date test 2', () => {
+  expect(isDate('3/25/2008')).toBe(true);
+});
+
+test('invalid date test 1', () => {
+  expect(isDate('AHHHHHHHHHHHHHHHHHHHHHHHH')).toBe(false);
+});
+
+test('invalid date test 2', () => {
+  expect(isDate('9/18/04')).toBe(false);
+});
+
+test('valid hex color 1', () => {
+  expect(isHexColor('FFC0CB')).toBe(true);
+});
+
+test('valid hex color 2', () => {
+  expect(isHexColor('FFB6C1')).toBe(true);
+});
+
+test('invalid hex color 1', () => {
+  expect(isHexColor('1234@5')).toBe(false);
+});
+
+test('invalid hex color 2', () => {
+  expect(isHexColor('meow1234')).toBe(false);
+});
+
+
+
+
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,46 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const playButton = document.querySelector("button");
+  const textInput = document.getElementById("text-to-speak");
+  const voiceSelect = document.getElementById("voice-select");
+  const img = document.querySelector("img");
+  let voices = []
+
+  function loadVoices() {
+    voices = speechSynthesis.getVoices();
+    const avail_voices = document.getElementById("voice-select");
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = index;
+      avail_voices.appendChild(option);
+    });
+  }
+
+  loadVoices();
+  speechSynthesis.addEventListener("voiceschanged", loadVoices);
+
+  playButton.addEventListener("click", function () {
+    const speak = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoice = voices[voiceSelect.value];
+
+    if(selectedVoice) {
+      speak.voice = selectedVoice;
+    }
+
+    speechSynthesis.speak(speak);
+
+    const checkSpeaking = setInterval(() => {
+      if (speechSynthesis.speaking) {
+        img.src = "assets/images/smiling-open.png";
+      } else {
+        img.src = "assets/images/smiling.png";
+        clearInterval(checkSpeaking);
+      }
+
+    }, 100);
+    
+  });
 }
